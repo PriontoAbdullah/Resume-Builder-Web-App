7f470c89d570a3b8b2c297878e0a172db0a8e635
unknown <yasmas169572274@gmail.com>
2020-07-24 11:00:22 +0600

    Features Done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './components/Landing-page/Header/Header';
 import Hero from './components/Landing-page/Hero/Hero';
+import Features from './components/Landing-page/Features/Features';
 
 function App() {
   return (
     <div className="">
       <Header />
       <Hero />
+      <Features />
     </div>
   );
 }
